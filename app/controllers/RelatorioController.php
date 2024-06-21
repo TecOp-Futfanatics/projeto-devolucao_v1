6f3b37514fbcbeb6f94acfee2dc445c8f81f4623
TecOp <rodrigo.froes@futcorp.com.br>
2024-06-21 15:01:30 -0300

    Feat: Criação de leitura de img

--- app/controllers/RelatorioController.php
+++ app/controllers/RelatorioController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace app\controllers;
+
 use app\models\OrigemModel;
 
 class RelatorioController
@@ -9,5 +11,25 @@ class RelatorioController
     $model = new OrigemModel("", "");
     $lista = $model->listarOrigem();
     Controller::view("devolucao/relatorio-rnc", ['lista' => $lista]);
+  }
+
+  public function store($params)
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      if (!empty($_FILES['image']['name'][0])) {
+        foreach ($_FILES['image']['name'] as $key => $name) {
+          if ($_FILES['image']['error'][$key] === 0) {
+            $tmp_name = $_FILES['image']['tmp_name'][$key];
+            $size = $_FILES['image']['size'][$key];
+            $type = $_FILES['image']['type'][$key];
+
+            echo "Arquivo: $name, Tamanho: $size bytes, Tipo: $type<br>";
+          }
+        }
+      } else {
+        echo "Nenhum arquivo foi enviado.";
+      }
+    }
+    die();
   }
 }
