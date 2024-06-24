722efe7bdeaee16f6d0e4689329581ee5ed7c141
TecOp <rodrigo.froes@futcorp.com.br>
2024-06-24 08:52:13 -0300

    Feat: Atualização Controller

--- app/controllers/RelatorioController.php
+++ app/controllers/RelatorioController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 use app\models\OrigemModel;
 
+
 class RelatorioController
 {
   public function index()
@@ -14,22 +15,6 @@ class RelatorioController
 
   public function store($params)
   {
-    var_dump($params);
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      if (!empty($_FILES['image']['name'][0])) {
-        foreach ($_FILES['image']['name'] as $key => $name) {
-          if ($_FILES['image']['error'][$key] === 0) {
-            $tmp_name = $_FILES['image']['tmp_name'][$key];
-            $size = $_FILES['image']['size'][$key];
-            $type = $_FILES['image']['type'][$key];
-
-            echo "Arquivo: $name, Tamanho: $size bytes, Tipo: $type<br>";
-          }
-        }
-      } else {
-        echo "Nenhum arquivo foi enviado.";
-      }
-    }
-    die();
+    print_r($params);
   }
 }
