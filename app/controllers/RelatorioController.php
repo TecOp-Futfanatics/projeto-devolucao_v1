32aff820da672f2db3630f0bfa0c5680361d6797
TecOp <rodrigo.froes@futcorp.com.br>
2024-06-25 17:02:16 -0300

    Feat: Alteração de validações e retorno da lista

--- app/controllers/RelatorioController.php
+++ app/controllers/RelatorioController.php
@@ -1,19 +1,42 @@
 <?php
+
 namespace app\controllers;
+
 use app\models\RelatorioOrigemModel;
+use app\models\FornecedorModel;
 
 class RelatorioController
 {
   public function index()
   {
     $model = new RelatorioOrigemModel("", "");
+    $modelFornecedor = new FornecedorModel("", "", "", "");
+    $fornecedor = $modelFornecedor->listarFornecedor();
     $lista = $model->listarOrigem();
-    Controller::view("devolucao/relatorio-rnc", ['lista' => $lista]);
+    Controller::view("devolucao/relatorio-rnc", ['lista' => $lista, 'fornecedor' => $fornecedor]);
   }
 
   public function store($params)
   {
-    echo json_encode($params);
+    var_dump($params);
+    die();
+    if (
+      $params->marca == "" || $params->fornecedor == "" ||
+      $params->cnpj == "" || $params->or == "" ||
+      $params->nf == "" || $params->emissao == "" ||
+      $params->dataAvaria == "" || $params->QuantReprovado == "" ||
+      $params->origem == "" || $params->nomeProduto == "" ||
+      $params->codigoRef == "" || $params->codigoFut == "" ||
+      $params->valorUnit == "" || $params->quantTotal == "" ||
+      $params->custo == "" || $params->variacao == "" || $params->quantidade == "" ||
+      $params->quantFaturado == "" || $params->detalhes == "" || $params->destinacao == "" ||
+      $params->image == ""
+    ) {
+      $error = "Preencha todos os campos";
+      Controller::view("devolucao/relatorio-rnc", ['error' => $error]);
+    } else {
+      $success = "Relatório cadastrado com sucesso"; 
+      Controller::view("devolucao/relatorio-rnc", ['success' => $success]);
+    }
   }
-
 }
