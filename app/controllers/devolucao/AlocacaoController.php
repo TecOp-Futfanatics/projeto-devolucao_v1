64f37497d62532b5b1cf8ef3db3c429818e7df56
TecOp <rodrigo.froes@futcorp.com.br>
2024-06-28 17:53:03 -0300

    Feat: Atualização controller

--- app/controllers/devolucao/AlocacaoController.php
+++ app/controllers/devolucao/AlocacaoController.php
@@ -4,6 +4,13 @@ use app\controllers\Controller;
 use app\models\devolucao\AlocacaoRncModel;
 
 class AlocacaoController {
+
+    private $contador;
+
+    public function __construct(){
+        $this->contador = 0;
+    }
+
     public function index(){
         Controller::view("devolucao/coletor/coletorAlocacao");
     }
@@ -12,7 +19,7 @@ class AlocacaoController {
     }
 
     public function store($request){
-        $model = new AlocacaoRncModel($request->rnc);
+        $model = new AlocacaoRncModel($request->rnc, "");
         $rnc = $model->buscarRnc();
         if($rnc){
             Controller::view("devolucao/coletor/consultaCodigoRnc", [
@@ -21,6 +28,39 @@ class AlocacaoController {
         } else {
             Controller::view("devolucao/coletor/alocacaoProduto", [
                 "msg" => "RNC não encontrado"
+            ]);
+        }
+    }
+
+    public function verificacao($params){
+        $model = new AlocacaoRncModel($params->rnc, "");
+        $quantProduto = $model->CountProduto();
+
+        Controller::view("devolucao/coletor/verificacaoProduto", [
+            "rnc" => $params->rnc,
+            "quantProduto" => $quantProduto,
+            "contador"  => $this->contador
+        ]);
+    }
+
+    public function verificacaoCodigo($params){
+        $model = new AlocacaoRncModel($params->rnc, "");
+        $quantProduto = $model->CountProduto();
+        $model = new AlocacaoRncModel($params->rnc, $params->codigo);
+        $resultado = $model->verificarCodigoProduto();
+        if($resultado){
+            Controller::view("devolucao/coletor/verificacaoProduto", [
+               "success" => "Código pertence a esta RNC",
+               "rnc" => $params->rnc,
+               "contador" => $this->contador+=1,
+               "quantProduto" => $quantProduto,
+            ]);
+        } else {
+            Controller::view("devolucao/coletor/verificacaoProduto", [
+                "error" => "Código não pertence a esta RNC",
+                "rnc" => $params->rnc,
+                "contador" => $this->contador,
+                "quantProduto" => $quantProduto
             ]);
         }
     }
