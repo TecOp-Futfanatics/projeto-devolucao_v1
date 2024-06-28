ca06c2daccc5afbe20ce9cfab67dba99a5949bcc
TecOp <rodrigo.froes@futcorp.com.br>
2024-06-28 13:52:27 -0300

    Feat: Atualização controller

--- app/controllers/devolucao/AlocacaoController.php
+++ app/controllers/devolucao/AlocacaoController.php
@@ -1,9 +1,27 @@
 <?php
 namespace app\controllers\devolucao;
 use app\controllers\Controller;
+use app\models\devolucao\AlocacaoRncModel;
 
 class AlocacaoController {
     public function index(){
-        Controller::view("devolucao/coletorAlocacao");
+        Controller::view("devolucao/coletor/coletorAlocacao");
+    }
+    public function rnc(){
+        Controller::view("devolucao/coletor/alocacaoProduto");
+    }
+
+    public function store($request){
+        $model = new AlocacaoRncModel($request->rnc);
+        $rnc = $model->buscarRnc();
+        if($rnc){
+            Controller::view("devolucao/coletor/consultaCodigoRnc", [
+                "rnc" => $rnc
+            ]);
+        } else {
+            Controller::view("devolucao/coletor/alocacaoProduto", [
+                "msg" => "RNC não encontrado"
+            ]);
+        }
     }
 }
